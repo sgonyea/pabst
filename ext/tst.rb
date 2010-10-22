93f13eed9afc4b4862b6a6c07e22690571f41805
Scott Gonyea <me@aitrus.org>
2010-10-22 08:25:26 -0700

    less typing in the tst.rb. kinda. eval is weird, yea?

--- ext/tst.rb
+++ ext/tst.rb
@@ -3,48 +3,33 @@ puts require './Riakpb'
 
 puts "initializing Riakpb::Pabst..."
 
-puts "a = Riakpb::Pabst.new"
-puts "=> #{a = Riakpb::Pabst.new}"
+puts %Q{a = Riakpb::Pabst.new}
+puts %Q{=> #{a = Riakpb::Pabst.new}}
 
-puts %Q{client_id = a.get_client_id}
-puts "=> #{client_id = a.get_client_id}"
+client_id = nil;
+server_info = nil;
+keys = nil;
+key = nil;
+put_key = nil;
+buckets = nil;
 
-puts %Q{client_id.class}
-puts "=> #{client_id.class}"
-
-puts %Q{client_id.size}
-puts "=> #{client_id.size}"
-
-puts %Q{server_info = a.server_info}
-puts "=> #{server_info = a.server_info}"
-
-puts %Q{server_info.class}
-puts "=> #{server_info.class}"
-
-puts %Q{server_info.size}
-puts "=> #{server_info.size}"
-
-puts %Q{keys = a.list_keys "tstBucket"}
-puts "=> #{keys = a.list_keys 'tstBucket'}"
-
-puts %Q{keys.class}
-puts "=> #{keys.class}"
-
-puts %Q{keys.length}
-puts "=> #{keys.length}"
-
-puts %Q{key = a.get_key "tstBucket", "tstKey"}
-puts "=> #{key = a.get_key 'tstBucket', 'tstKey'}"
-
-puts %Q{key.class}
-puts "=> #{key.class}"
-
-puts %Q{key.length}
-puts "=> #{key.length}"
-
-puts %Q{a.put_key "tstBucket", "tstKey3", {:value => "threezy peazy!", :content_type => "crap"}, nil, 1, 1, true}
-puts %Q{=> #{a.put_key "tstBucket", "tstKey3", {:value => "threezy peazy!", :content_type => "crap"}, nil, 1, 1, true}}
-
-puts %Q{buckets = a.list_buckets}
-puts %Q{=> #{buckets = a.list_buckets}}
-
+[ %Q{client_id = a.get_client_id},
+  %Q{client_id.class},
+  %Q{client_id.size},
+  %Q{server_info = a.server_info},
+  %Q{server_info.class},
+  %Q{server_info.size},
+  %Q{keys = a.list_keys "tstBucket"},
+  %Q{keys.class},
+  %Q{keys.length},
+  %Q{key = a.get_key "tstBucket", "tstKey"},
+  %Q{key.class},
+  %Q{key.length},
+  %Q{a.put_key "tstBucket", "tstKey3", {:value => "threezy peazy!", :content_type => "crap"}, nil, 1, 1, true},
+  %Q{buckets = a.list_buckets}
+].each do |crap_test|
+  
+  puts crap_test
+  puts %Q{=> #{eval crap_test}}
+  
+end
